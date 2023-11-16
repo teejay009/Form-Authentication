a1a23ad7930f809895df4fee3061c998f31f32a9
teejay009 <tajdjimoh@gmail.com>
2023-11-16 11:20:03 +0100

    update

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
  import useAxiosPrivate from "../hooks/useAxiosPrivate";
- import { useNavigate, useLocation } from "react-router-dom";
+ import { useNavigate,  } from "react-router-dom";
 
 
 
